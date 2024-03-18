5149c3d8c473d901f4b4b6b873230b05f5887d12
Basiak2i <102364757+Basiak2i@users.noreply.github.com>
2024-03-18 10:24:18 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,51 +3,49 @@ const miasto = document.querySelector('#Miasto');
 const wiek = document.querySelector('#Wiek');
 const dodaj = document.querySelector('#dodaj');
 const display = document.querySelector('#display');
+const wartosc = document.querySelector('#wartosc');
+const kryterium = document.querySelector('#kryterium');
+const wyszukaj = document.querySelector('#wyszukaj'); 
+const lista2 = document.querySelector('#lista2');
 
+display.innerHTML = "";
+aktualizujListe(lista);
+function aktualizujListe(lista) {
+    display.innerHTML = "";
+    lista.forEach((el, i) => {
+        const li = document.createElement('li');
+        const btn = document.createElement('button');
+        btn.classList.add("btn", "btn-outline-danger");
+        btn.addEventListener('click', function(){
+            usun(el.id);
+        });
+        btn.innerHTML = 'x';
+        li.innerHTML += `<strong>${el.imie}</strong> lat <strong>${el.wiek}</strong> z miasta <strong>${el.miasto}</strong>`;
+        li.classList.add("list-group-item");
+        li.append(btn);
+        display.append(li);
+    });
+}
 
-display.innerHTML = ""
-lista.forEach((el, i) => {
-    const li = document.createElement('li')
-    const btn = document.createElement('button')
-    btn.classList.add("btn", "btn-outline-danger")
-    btn.id = i;
-    btn.addEventListener('click',function (evt){
-        console.log(this.id)
-    })
-    btn.innerHTML = 'x'
-    li.innerHTML += `<strong>${el.imie}</strong> lat <strong>${el.wiek}</strong> z miasta <strong>${el.miasto}</strong>`
-    li.classList.add("list-group-item")
-    li.append(btn)
-    display.append(li)
+function usun(removeByID) {
+    lista = lista.filter((user) => user.id !== removeByID);
+    aktualizujListe(lista);
+}
+
+dodaj.addEventListener('click', evt => {
+    let uzytkownik = {
+        id: crypto.randomUUID(),
+        imie: imie.value,
+        miasto: miasto.value,
+        wiek: wiek.value
+    };
+    lista.unshift(uzytkownik);
+    aktualizujListe(lista);
+    
 });
 
-            dodaj.addEventListener('click', evt => {
+// wyszukaj.addEventListener('click', function(evt){
+//     evt.preventDefault();
 
-                let uzytkownik = {
-                    id: crypto.randomUUID(),
-                    imie: imie.value,
-                    miasto: miasto.value,
-                    wiek: wiek.value
-                
-                }
-                lista.unshift(uzytkownik)
-                console.log(lista)
-                
-                display.innerHTML = ""
-                lista.forEach((el, i) => {
-                    const li = document.createElement('li')
-                    const btn = document.createElement('button')
-                    btn.classList.add("btn", "btn-outline-danger")
-                    btn.id = i;
-                    btn.addEventListener('click',function (evt){
-                        console.log(this.id)
-                    })
-                    btn.innerHTML = 'x'
-                    li.innerHTML += `<strong>${el.imie}</strong> lat <strong>${el.wiek}</strong> z miasta <strong>${el.miasto}</strong>`
-                    li.classList.add("list-group-item")
-                    li.append(btn)
-                    display.append(li)
-                    
-                });
-
-            })
+//     lista2.innerHTML="";
+// })
